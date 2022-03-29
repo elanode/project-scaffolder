6ce0c375d26dfc469511413b1be36684353f5a1b
Jason Chan <jasonnchann24@gmail.com>
2022-03-30 03:09:18 +0700

    chore(api): routes, full namespace class

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,24 +1,12 @@
 <?php
 
-use App\Http\Controllers\V1\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::post('/logout', LogoutController::class);
+    Route::post('/logout',  App\Http\Controllers\V1\Auth\LogoutController::class);
 });
